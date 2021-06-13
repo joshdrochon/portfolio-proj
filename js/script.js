d8c302e4e61063225e2a79c371dec081c09a6401
Josh Rochon <joshdrochon@gmail.com>
2021-06-12 20:15:21 -0700

    refactor collision detection

--- js/script.js
+++ js/script.js
@@ -2,66 +2,119 @@ const HEADERTEXT = "Hi. I'm Josh."
 let i = 0
 
 class Sprite {
-    constructor(x, y) {
+    constructor(x, y, relativeX = 0, relativeY = 0) {
         this.x = x;
         this.y = y
+        this.relativeX = relativeX
+        this.relativeY = relativeY
+    }
+    moveLeft() {
+        this.relativeX = +(this.relativeX - .2).toFixed(1)
+    }
+    moveRight() {
+        this.relativeX = +(this.relativeX + .2).toFixed(1)
     }
 }
 
-//here position
-const JOSH = new Sprite(250, 250)
+const rainSound = new Audio('./assets/audio/rain-and-thunder.mp3')
+rainSound.volume = .5
+const themeMusic = new Audio('./assets/audio/theme-music.mp3')
+themeMusic.loop = true
+
+let hero
+
+//hero position
+const JOSH = new Sprite(100, 250, 2, 5)
 //canvas position
 const GAMEWORLD = new Sprite(0, 0)
 
 const numOfRows = 8
 const numOfCols = 36
 const sqSize = 50
-const emptySq = '#0597f2'
+const emptySq = '#7393b3'
 
 function createGameWorld() {
+    hero = document.getElementById('hero')
+
+    hero.style.backgroundImage =  "url('./assets/img/pixeljosh.png')";
+
     const canvas = document.querySelector('#game-world')
     const context = canvas.getContext('2d')
     drawWorld(context)
 }
 
-function drawSquares(x, y, color, ctx) {
-    if (y == 7) {
+let gameGrid = []
+for (let r = numOfRows - 1; r >= 0; r-=1){
+    gameGrid[r] = []
+    for (let c = numOfCols - 1; c >= 0; c-=1){
+        //empty = 0
+        //soil = 1
+        //grass = 2
+        gameGrid[r][c] = 0
+        if (r == 6) {
+            //soil below
+            if (gameGrid[r+1][c] == 1) {
+                //place grass above
+                gameGrid[r][c] = 2
+            }
+        }
+        if (r == 7) {
+            //50% chance of truthy
+            if (Math.floor(Math.random()*2))
+                //grass or soil
+                gameGrid[r][c] = Math.floor(Math.random() * 2) + 1
+            else {
+                //grass
+                gameGrid[r][c] = 2
+            }
+        }
+    }
+}
+
+console.log(gameGrid)
+
+function drawWorld(context) {
+    for (let r = 0; r < numOfRows; r++){
+        for (let c = 0; c < numOfCols; c++){
+            drawSquares(c, r, gameGrid[r][c], context)
+        }
+    }
+}
+
+function drawSquares(x, y, pattern, ctx) {
+    //grass
+    if (pattern == 2) {
+        let grass = new Image()
+        grass.src = "./assets/img/pixelgrass.png"
+        grass.onload = function() {
+            ctx.drawImage(this, x*sqSize, y*sqSize, 50, 50)
+        }
+    }
+    //soil
+    if (pattern == 1) {
         let dirt = new Image()
-        dirt.src = "./assets/img/pixelgrass.png"
+        dirt.src = "./assets/img/pixelblock.png"
         dirt.onload = function() {
             ctx.drawImage(this, x*sqSize, y*sqSize, 50, 50)
         }
     }
-    else if (y == 3 && x == 5) {
-        let js = new Image()
-        js.src = "./assets/img/pixeljavascript.png"
-        js.onload = function() {
-            ctx.drawImage(this, x*sqSize, y*sqSize, 50, 50)
-        }
+    //empty
+    if (pattern == 0) {
+        ctx.fillStyle = emptySq
     }
-    else {
-        ctx.fillStyle = color
-    }
+    
+    // if (y == 3 && x == 5) {
+    //     let js = new Image()
+    //     js.src = "./assets/img/pixeljavascript.png"
+    //     js.onload = function() {
+    //         ctx.drawImage(this, x*sqSize, y*sqSize, 50, 50)
+    //     }
+    // }
+
+
     ctx.fillRect(x*sqSize, y*sqSize, sqSize, sqSize)
     // ctx.strokeStyle = "#000000"
     ctx.strokeRect(x*sqSize, y*sqSize, sqSize, sqSize)
-}
-
-let board = []
-for (let r = 0; r < numOfRows; r++){
-    board[r] = []
-    for (let c = 0; c < numOfCols; c++){
-        board[r][c] = emptySq
-    }
-}
-
-function drawWorld(context){
-    
-    for (let r = 0; r < numOfRows; r++){
-        for (let c = 0; c < numOfCols; c++){
-            drawSquares(c, r, board[r][c], context)
-        }
-    }
 }
 
 function typeWriter() {
@@ -112,6 +165,8 @@ function handleClick(){
 }
 
 function initGame() {
+    rainSound.play()
+    themeMusic.play()
     document.getElementById('story').style.display = "block"
     createGameWorld()
     document.addEventListener("keydown", control)
@@ -120,28 +175,36 @@ function initGame() {
 function control(e) {
     e.preventDefault()
 
-    let hero = document.getElementById("hero"),
-    gameworld = document.getElementById("game-world"),
+    let gameworld = document.getElementById("game-world"),
     rain = document.getElementById("rain")
 
     if (e.keyCode === 32) {
         console.log("i am jumping")
     }
+    
     //move left
     if (e.keyCode === 37) {
         hero.style.transform = "scaleX(-1)"
-        if (GAMEWORLD.x === 0) {
-            if (JOSH.x > 0) {
-                JOSH.x -= 10
-            }
-        }
-        if (GAMEWORLD.x < 0) {
-            if (JOSH.x === 250) {
-                GAMEWORLD.x += 10
-            }
-            if (GAMEWORLD.x === -1200) {
-                if (JOSH.x > 250) {
+
+        let nextLeftPosition = Math.ceil(JOSH.relativeX) - 1
+        
+        if (gameGrid[JOSH.relativeY + 1][nextLeftPosition] === 0) {
+            JOSH.moveLeft()
+            hero.style.backgroundImage = `url('./assets/img/pixeljosh${hero.style.backgroundImage.indexOf("2") > -1 ? "" : "2"}.png')`
+        
+            if (GAMEWORLD.x === 0) {
+                if (JOSH.x > 0) {
                     JOSH.x -= 10
+                }
+            }
+            if (GAMEWORLD.x < 0) {
+                if (JOSH.x === 250) {
+                    GAMEWORLD.x += 10
+                }
+                if (GAMEWORLD.x === -1200) {
+                    if (JOSH.x > 250) {
+                        JOSH.x -= 10
+                    }
                 }
             }
         }
@@ -149,22 +212,29 @@ function control(e) {
     //move right
     if (e.keyCode === 39) {
         hero.style.transform = "scaleX(1)"
-        if (JOSH.x >= 250) {
-            if (GAMEWORLD.x === -1200) {
-                if (JOSH.x < 550) {
-                    JOSH.x += 10
+
+        let nextRightPosition = Math.floor(JOSH.relativeX) + 1
+
+        if (gameGrid[JOSH.relativeY + 1][nextRightPosition] === 0) {
+            JOSH.moveRight()
+            hero.style.backgroundImage = `url('./assets/img/pixeljosh${hero.style.backgroundImage.indexOf("2") > -1 ? "" : "2"}.png')`
+        
+            if (JOSH.x >= 250) {
+                if (GAMEWORLD.x === -1200) {
+                    if (JOSH.x < 550) {
+                        JOSH.x += 10
+                    }
+                }
+                if (GAMEWORLD.x > -1200) {
+                    GAMEWORLD.x -= 10
                 }
             }
-            if (GAMEWORLD.x > -1200) {
-                GAMEWORLD.x -= 10
+            if (JOSH.x < 250) {
+                JOSH.x += 10
             }
         }
-        if (JOSH.x < 250) {
-            JOSH.x += 10
-        }
+        
     }
-    console.log("WORLD:", GAMEWORLD.x)
-    console.log("JOSH:", JOSH.x)
     hero.style.left = JOSH.x + 'px'
     hero.style.top = JOSH.y + 'px'
     gameworld.style.left = GAMEWORLD.x + 'px'
