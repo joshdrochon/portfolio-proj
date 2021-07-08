af57fe995822dfc8d3223b2367017d4e3029839c
Josh Rochon <joshdrochon@gmail.com>
2021-07-07 23:34:35 -0700

    put all typewriter logic in one function, refactor pattern styles, add images, and display to canvas with function, double world length, and refactor

--- js/script.js
+++ js/script.js
@@ -1,36 +1,77 @@
-let debugMode = false
+let skills = [,,,'javascript', 'csharp', 'html5', 'css3', 'github', 'mysql','python','react', 'bitbucket']
 
-const HEADERTEXT = "Hi. I'm Josh."
-let i = 0
+let cursorInterval,
+cursorTimeout
+
+function cursorBlink() {
+    let cursor = document.getElementById("cursor")
+    if (cursor.classList.contains("invisible")) {
+        cursor.classList.remove("invisible")
+        return
+    }
+    cursor.classList.add("invisible")
+}
+
+(function typeWriter(text) {
+    let i = 0
+
+    function step() {
+        if (i < text.length) {
+            let welcome = document.getElementById("welcome-text")
+            welcome.innerHTML += text.charAt(i)
+    
+            let cursor = document.getElementById("cursor"),
+    
+                bClientRect = welcome.getBoundingClientRect()
+    
+            cursor.style.left = text.charAt(i) == " " ?
+                bClientRect.width + 10 + 'px' :
+                bClientRect.width + 'px'
+    
+            i += 1
+            setTimeout(step, i == 3 ? 300 : 100)
+        }
+    }
+
+    window.setTimeout(() => {
+        step()
+        cursorTimeout = window.setTimeout(() => {
+            cursorInterval = window.setInterval(cursorBlink, 530)
+        }, 1500)
+    }, 500)
+})("Hi. I'm Josh.")
+
+function handleClick() {
+    let welcomeCont = document.getElementsByClassName('welcome-cont')[0]
+    welcomeCont.style.display = "none"
+    window.clearInterval(cursorInterval)    
+    window.clearTimeout(cursorTimeout)
+    initGame()
+}
+
+//collected skills
+let collectables = []
 
 const WORLD_TILES = {
     0: '#7393b3',
     1: new Image(),
     2: new Image(),
-    3: new Image(),
 }
-
-const SKILL_TILES = {}
-
-let tileCoords = []
-
-// function randomize() {
-//     for (let i = 0; i < 1; i+=1) {
-//         let img = new Image()
-//         img.src = './assets/img/pixeljavascript.png'
-//         SKILL_TILES[i] = img
-//     }
-// }
-
-// randomize()
-
-let rainBackground
 
 WORLD_TILES["1"].src = './assets/img/pixelblock.png'
 WORLD_TILES["2"].src = './assets/img/pixelgrass.png'
-WORLD_TILES["3"].src = './assets/img/pixeljavascript.png'
 
-console.log(WORLD_TILES)
+function createSkills() {
+    for (let i = 3; i < skills.length; i+=1) {
+        let img = new Image()
+        img.src = `./assets/img/pixel${skills[i]}.png`
+        WORLD_TILES[i] = img
+    }
+}
+
+createSkills()
+
+let rainBackground
 
 const HERO_IMAGES = {
     0: new Image(),
@@ -51,7 +92,7 @@ let hero,
 worldXPosition = 0
 
 const numOfRows = 8
-const numOfCols = 36
+const numOfCols = 72
 const sqSize = 50
 const emptySq = '#7393b3'
 
@@ -63,7 +104,7 @@ function createGameWorld() {
     drawWorld()
 }
 
-let gameGrid = []
+let gameGrid = [], skillsIndex = 3
 for (let r = numOfRows - 1; r >= 0; r -= 1) {
     gameGrid[r] = []
     for (let c = numOfCols - 1; c >= 0; c -= 1) {
@@ -74,8 +115,8 @@ for (let r = numOfRows - 1; r >= 0; r -= 1) {
         gameGrid[r][c] = 0
 
         if (r== 3) {
-            if (c == 2 || c == 5 || c == 9 || c == 17 || c == 21) {
-                gameGrid[r][c] = 3
+            if (c==2||c==9||c==15||c==27||c==39||c==48||c==54||c==65||c==70) {
+                gameGrid[r][c] = skillsIndex++
             }
         }
         if (r == 6) {
@@ -109,31 +150,14 @@ function drawWorld() {
 
 function drawSquares(x, y, pattern) {
     context.fillRect(x * sqSize, y * sqSize, sqSize, sqSize)
-    //empty
-    if (pattern == 0) {
-        context.fillStyle = WORLD_TILES["0"]
-    }
-    //soil
-    if (pattern == 1) {
-        context.drawImage(WORLD_TILES["1"], x * sqSize, y * sqSize, 50, 50)
-    }
-    //grass
-    if (pattern == 2) {
-        context.drawImage(WORLD_TILES["2"], x * sqSize, y * sqSize, 50, 50)
-    }
-    //skill
-    if (pattern == 3) {
-        context.drawImage(WORLD_TILES["3"], x * sqSize, y * sqSize, 50, 50)
-    }
-    //TODO remove before deployment
-    // context.strokeStyle = "black"
-    // context.strokeRect(x * sqSize, y * sqSize, sqSize, sqSize)
-    //TODO remove before deployment
-    // context.strokeStyle = "#FF0000"
+    
+    pattern > 0 ?
+    context.drawImage(WORLD_TILES[pattern], x * sqSize, y * sqSize, 50, 50) : 
+    context.fillStyle = WORLD_TILES[pattern]
 }
 
 function drawPlayer() {
-    // context.strokeRect(player.x, player.y, 50, 100)
+    context.strokeRect(player.x, player.y, 50, 100)
     //image comes add odd dimensions
     context.drawImage(HERO_IMAGES[player.heroStance], player.x - 15, player.y - 10)
 }   
@@ -179,39 +203,27 @@ function initGame() {
         right: false,
         up: false,
         down: false,
-        keyListener: function (event) {
-            let keyMap = {}
-            keyMap[event.keyCode] = event.type == "keydown"
-            
-            let key_state = event.type == "keydown" ? 
-            true : 
-            false;
 
-            switch (event.keyCode) {
+        keyListener (e) {
+            e.preventDefault()
+
+            let key_state = e.type == "keydown"
+            switch (e.keyCode) {
                 case 37:
-                    event.preventDefault()
                     controller.left = key_state;
                     break;
                 case 32:
-                    event.preventDefault()
                     JUMPSOUND.play()
                     controller.up = key_state;
                     break;
                 case 40:
-                    event.preventDefault()
                     controller.down = key_state;
                     break;
                 case 39:
-                    event.preventDefault()
                     controller.right = key_state;
                     break;
-                default:
-                    event.preventDefault()
-
             }
-
         }
-
     };
 
     gameLoop = function () {
@@ -259,43 +271,51 @@ function initGame() {
         if (player.x < 250) {
             worldXPosition = 0
         }
-        
-        let playerGridX = Math.ceil(player.x /50)
-        let playerGridY = Math.floor(player.y /50) + 1
+
+        //player grid locations
+        let pGridX = Math.ceil(player.x /50)
+        let pGridY = Math.floor(player.y /50) + 1
 
         //player left of block
-        if (gameGrid[playerGridY][playerGridX] == 2) {
+        if (gameGrid[pGridY][pGridX] == 2) {
             player.x_velocity = 0
-            player.x = (playerGridX * 50) - 50
+            player.x = (pGridX * 50) - 50
             controller.right = false
         }
         //player right of block
-        if (gameGrid[playerGridY][playerGridX -1] == 2) {
+        if (gameGrid[pGridY][pGridX -1] == 2) {
             player.x_velocity = 0
-            player.x = (playerGridX * 50)
+            player.x = (pGridX * 50)
             controller.left = false
         }
 
         //player top of block
-        if (gameGrid[playerGridY+1][playerGridX] == 2 || gameGrid[playerGridY+1][playerGridX -1] == 2) {
-            if ((playerGridX * 50) - player.x > 0) {
+        if (gameGrid[pGridY+1][pGridX] == 2 || gameGrid[pGridY+1][pGridX -1] == 2) {
+            if ((pGridX * 50) - player.x > 0) {
                 player.jumping = false;
                 player.y_velocity = 0
                 if (player.y < 250) {
-                    player.y = (playerGridY * 50) - 50
+                    player.y = (pGridY * 50) - 50
                 }
             }
         }
 
-        if (gameGrid[playerGridY - 1][playerGridX] == 3) {
-            COLLECTSOUND.play()
-            gameGrid[playerGridY - 1][playerGridX] = 0
-        }
-        if (gameGrid[playerGridY - 1][playerGridX - 1] == 3) {
-            COLLECTSOUND.play()
-            gameGrid[playerGridY - 1][playerGridX - 1] = 0
-        }
+        //skill collision
+        if ((gameGrid[pGridY - 1][pGridX] || gameGrid[pGridY - 1][pGridX - 1]) > 2) {
 
+            let collectable = gameGrid[pGridY - 1][pGridX] || 
+            gameGrid[pGridY - 1][pGridX - 1]
+
+            collectables.push(collectable)
+
+            gameGrid[pGridY - 1][pGridX - 1] === 0 ? 
+            gameGrid[pGridY - 1][pGridX] = 0 : 
+            gameGrid[pGridY - 1][pGridX - 1] = 0
+
+            COLLECTSOUND.play()
+
+            console.log(collectables)
+        }
 
         rainBackground.style.left = worldXPosition + 'px'
         canvas.style.left = worldXPosition + 'px'
@@ -303,9 +323,7 @@ function initGame() {
         drawWorld()
         drawPlayer()
 
-        setTimeout(() => {
-            requestAnimationFrame(gameLoop);
-        }, debugMode ? 500 : 1);
+        requestAnimationFrame(gameLoop);
     }
 
     window.addEventListener("keydown", controller.keyListener)
@@ -316,71 +334,5 @@ function initGame() {
 
 
 
-
-
-
-
-
-
-
-//first screen
-function typeWriter() {
-    if (i < HEADERTEXT.length) {
-        let welcome = document.getElementById("welcome-text")
-        welcome.innerHTML += HEADERTEXT.charAt(i)
-
-        let cursor = document.getElementById("cursor"),
-
-            bClientRect = welcome.getBoundingClientRect()
-
-        cursor.style.left = HEADERTEXT.charAt(i) == " " ?
-            bClientRect.width + 10 + 'px' :
-            bClientRect.width + 'px'
-
-        i += 1
-        setTimeout(typeWriter, i == 3 ? 300 : 100)
-    }
-}
-
-let cursorInterval;
-let cursorTimeout;
-
-window.setTimeout(() => {
-    typeWriter()
-    cursorTimeout = window.setTimeout(() => {
-        cursorInterval = window.setInterval(cursorBlink, 530)
-    }, 1500)
-}, 500)
-
-
-
-function cursorBlink() {
-    let cursor = document.getElementById("cursor")
-    if (cursor.classList.contains("invisible")) {
-        cursor.classList.remove("invisible")
-        return
-    }
-    cursor.classList.add("invisible")
-}
-
-function handleClick() {
-    let welcomeCont = document.getElementsByClassName('welcome-cont')[0]
-    welcomeCont.style.display = "none"
-    window.clearInterval(cursorInterval)    
-    window.clearTimeout(cursorTimeout)
-    initGame()
-}
-
-document.getElementById('debug').addEventListener('click', setDebugMode)
-
-function setDebugMode(e) {
-    e.preventDefault()
-    if (debugMode == true) {
-        debugMode = false
-    }
-    else {
-        debugMode = true
-    }
-}
 
 
